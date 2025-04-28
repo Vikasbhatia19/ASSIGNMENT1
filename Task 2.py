d845240aabd6ea2eb3e9ea72bff976f06c621215
Vikasbhatia19 <122993526+Vikasbhatia19@users.noreply.github.com>
2025-04-28 17:07:56 -0400

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 29 02:11:36 2025
+
+@author: vvtbh
+"""
+
+a=str(input("Please enter your first Name:->"))
+b=str(input("Please enter your last Name:->"))
+n=str(a+" "+b)
+print( 'Hi,', n,',Welcome to python world')
---
+++ Task1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a=float(input("Enter First Number:->"))
+b=float(input("enter Second Number:->"))
+Addition= a+b
+Subtraction=a-b
+Div=a/b
+Multi=a*b
+print("\Results:->")
+print("Addition", Addition)
+print("Subtraction", Subtraction)
+print("DIV", Div)
+print("Multi", Multi)
